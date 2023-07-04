ea8a9942c2bcd9eb032d7420049680b8d9d81421
Zhuo <hudirybw@gmail.com>
2023-07-04 15:24:19 +0200

    half

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.urls import reverse_lazy
+
+from ads.forms import CreateForm
 
 # Create your views here.
 from ads.models import Ad
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
-
 
 class AdListView(OwnerListView):
     model = Ad
@@ -14,17 +18,60 @@ class AdListView(OwnerListView):
 class AdDetailView(OwnerDetailView):
     model = Ad
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    # List the fields to copy from th
-    # e Ad model to the Ad form
-    fields = ['title', 'text', 'price']
+class AdCreateView(LoginRequiredMixin, View):
+    # model = Ad
+    # # List the fields to copy from th
+    # # e Ad model to the Ad form
+    # fields = ['title', 'text', 'price']
+    template_name = 'ads/form.html'
+    success_url = reverse_lazy('ads:all')
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'text', 'price']
-    # This would make more sense
-    # fields_exclude = ['owner', 'created_at', 'updated_at']
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        # Add owner to the model before saving
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+        return redirect(self.success_url)
+
+
+class AdUpdateView(LoginRequiredMixin, View):
+    # model = Ad
+    # fields = ['title', 'text', 'price']
+    # # This would make more sense
+    # # fields_exclude = ['owner', 'created_at', 'updated_at']
+    template_name = 'ads/form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+
+        return redirect(self.success_url)
+
 
 
 class AdDeleteView(OwnerDeleteView):
